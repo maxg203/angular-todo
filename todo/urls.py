82cf997b685ca3ce8101d1b7974dbe8f96c96638
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-06-07 02:48:51 +0100

    Added todo app created by startproject and url to point to template added

---
+++ todo/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='index.html'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
